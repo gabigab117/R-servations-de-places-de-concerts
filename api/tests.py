75e0c57d7de666a928ba7dd1ad3113f21aff0671
gabigab117 <gabrieltrouve5@outlook.com>
2023-05-03 23:40:10 +0200

    first_test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse_lazy
+from accounts.models import Shopper
 
-# Create your tests here.
+
+class TestShopper(APITestCase):
+    # list est la complétude faite par le routeur
+    url = reverse_lazy('api:shopper-list')
+
+    def test_shopper_not_shown_if_not_isactive(self):
+        user: Shopper = Shopper.objects.create_user(email="denine@denine.com",
+                                                    genre="nr",
+                                                    first_name="Denis",
+                                                    last_name="Noone",
+                                                    password="123456789",
+                                                    is_active=False)
+
+        user_2: Shopper = Shopper.objects.create_user(email="denine2@denine.com",
+                                                      genre="nr",
+                                                      first_name="Denis",
+                                                      last_name="Noone",
+                                                      password="123456789",
+                                                      is_active=True)
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(user.email, str(response.content))
+        self.assertIn(user_2.email, str(response.content))
